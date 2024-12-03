d23f84e71b19effa6509d1c2f5caf78ffc133a48
ilgarguliyevv <s.aliyev1214@gmail.com>
2024-12-03 17:14:31 +0400

    +

--- services/register.js
+++ services/register.js
@@ -1,25 +1,25 @@
 document.getElementById('registerForm').addEventListener('submit', async function(event) {
-  event.preventDefault(); // Prevents default form submission
+  event.preventDefault(); 
 
   const fullname = document.getElementById('fullname').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   const imagefile = document.getElementById('imagefile').files[0];
-  const fullnameRegex = /^[A-Za-z\s]+$/; // Only letters and spaces
+  const fullnameRegex = /^[A-Za-z\s]+$/; 
 
   if (!fullnameRegex.test(fullname)) {
       alert("Full name must contain only letters and spaces");
       return;
   }
 
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Standard email format
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; 
 
   if (!emailRegex.test(email)) {
       alert("Please enter a valid email address");
       return;
   }
 
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/; // Minimum 8 characters, 1 uppercase, 1 lowercase, 1 number
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/; 
 
   if (!passwordRegex.test(password)) {
       alert("Password must be at least 8 characters long, with at least one uppercase letter, one lowercase letter, and one number.");
@@ -37,10 +37,10 @@ document.getElementById('registerForm').addEventListener('submit', async functio
               password: password,
               profileImage: profileImage,
               createdAt: new Date().toISOString(),
-              experienceYear: 0, // Default value; adjust as needed
-              linkedinUrl: "", // Default value; can be set later
-              bio: "", // Default value; can be set later
-              skills: [] // Default value; can be set later
+              linkedinUrl: "", 
+              experienceYear: 0, 
+              bio: "", 
+              skills: [] 
           };
 
           try {
@@ -64,10 +64,10 @@ document.getElementById('registerForm').addEventListener('submit', async functio
           password: password,
           profileImage: null,
           createdAt: new Date().toISOString(),
-          experienceYear: 0, // Default value; adjust as needed
-          linkedinUrl: "", // Default value; can be set later
-          bio: "", // Default value; can be set later
-          skills: [] // Default value; can be set later
+          experienceYear: 0, 
+          linkedinUrl: "", 
+          bio: "", 
+          skills: [] 
       };
 
       try {
