9549b66354c876fc4c9455a0dc084b3a9e4e9d59
OqtayHasanli <OqtayHasanli>
2024-12-03 18:48:09 +0400

    hazir

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-let b;
+
+const user = document.querySelector(".side");
+const foruser = document.querySelector(".forUser");
+const inside = document.querySelector(".forUser .inside");
+inside.style.top = "-88px"
+
+user.addEventListener("click", function () {
+    console.log(foruser);
+
+    if (inside.style.top !== "-88px") {
+
+        inside.style.top = "-88px";
+    } else {
+        console.log("b");
+
+        inside.style.top = "170px";
+    }
+});
+
+
+
+
+
+
